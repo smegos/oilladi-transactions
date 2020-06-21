c70b37eb02e7bec0adfa1f80cbe7b797e465f069
Stephen Megos <64034938+smegos@users.noreply.github.com>
2020-06-21 12:46:31 -0400

    Update transaction_analysis.py

--- transaction_analysis.py
+++ transaction_analysis.py
@@ -27,21 +27,47 @@ sortedFile = open(newFile, "w")
 purchases = 0
 returns = 0
 
+dInventory = dict()
+dPayment = dict()
+dRefund = dict()
+
 for line in sourceFile:
     try:
         fields = line.split("\t")
         sortedFile.write(str(Transaction(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8])))
         transactionList.append(Transaction(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8]))
-        if "UPPER" in fields[3].upper():
-            purchases += 1
-        if "REFUND" in fields[3].upper():
-            returns += 1
+        if "PAYMENT" in fields[3].upper():
+            if fields[8] in dPayment:
+                dPayment[fields[8]] = dPayment[fields[8]] + 1
+            else:
+                dPayment[fields[8]] = 1
+        elif "REFUND" in fields[3].upper():
+            if fields[8] in dRefund:
+                dRefund[fields[8]] = dRefund[fields[8]] + 1
+            else:
+                dRefund[fields[8]] = 1
+        else:
+            sortedFile.write("No purchase or Refund detected in this transaction.\n")
 
     except IndexError:
         sortedFile.write("Insufficient information in transaction listing.\n")
 
 
+for key in list(dPayment.keys()):
+    print(key, "Purchases: ", dPayment[key])
+    sortedFile.write((key), "Purchases: ", str(dPayment[key]))
+
+for key in list(dRefund.keys()):
+    print(key, "Refunds: ", dRefund[key])
+    sortedFile.write(key, "Refunds: ", dRefund[key])
+
+for key in list(dPayment.keys()) and list(dRefund.keys()):
+    print(key, "Net Sales: ", dPayment[key] - dRefund[key])
+    sortedFile.write(key, "Net Sales: ", dPayment[key] - dRefund[key])
+
 inventory = purchases - returns
+
 sortedFile.write("This dataset shows: " + str(purchases) + " purchases, and " + str(returns) + " returns. Which shows " + str(inventory) + " net sales.")
 
 sourceFile.close()
+sortedFile.close()
